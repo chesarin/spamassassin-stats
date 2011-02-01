2da2f4bce0056f9f0f21647b707a980869d7eb42
chesarin <vargasces@gmail.com>
2011-02-01 16:40:45 -0500

    commenting out printing

--- logstats.py
+++ logstats.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
-import bz2
+import bz2;
 
-udata = bz2.BZ2File("spamlog.20101222.bz2",'r')
+udata = bz2.BZ2File("spamlog.20101222.bz2",'r');
 sentinel=0;
 
 for line in udata:
-    sentinel += 1
+    sentinel += 1;
+    if sentinel == 1:
+        line1 = line;
+    elif sentinel == 2:
+        line2 = line;
 
-print "The total number of lines is ",sentinel
-udata.close()
+#print line1;
+#print line2;
+
+#print "The total number of lines is ",sentinel;
+udata.close();
 
 
 
