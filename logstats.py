30c1d34aa6423a62407393bfd2f35229bea25c6b
chesarin <vargasces@gmail.com>
2011-03-22 16:14:43 -0400

    more ideas

--- logstats.py
+++ logstats.py
@@ -21,7 +21,13 @@ for line in udata:
 #print "The total number of lines is ",sentinel1 ," And the number of smtpfilter lines is ",sentinel2;
 
 udata.close();
-
+#def get_id(line):
+#On each line get the servernaem and the smtpfilter id, the 4th and 5th Column
+#We need to create a hash where these unique identifier can be stored.
+#Once that is done store them on the hash and for each new one check the hash so 
+#we don't store any duplicates. This will give us the total emails check on all the
+#sys_spam hosts.
+#matchObj = re.match(r'       
 
 
 
