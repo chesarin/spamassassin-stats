385c899d4c2233e0f130a0a1ee7125872685f99c
chesarin <vargasces@gmail.com>
2011-03-23 14:49:40 -0400

    more ideas to grab important data from smtpfilter logs

--- logstats.py
+++ logstats.py
@@ -6,6 +6,8 @@ udata = bz2.BZ2File("spamlog.20101222.bz2",'r');
 sentinel1=0;
 sentinel2=0;
 
+email_list = [];
+
 for line in udata:
     sentinel1 += 1;
     if re.search('smtpfilter',line):
@@ -21,15 +23,30 @@ for line in udata:
 #print "The total number of lines is ",sentinel1 ," And the number of smtpfilter lines is ",sentinel2;
 
 udata.close();
-#def get_id(line):
+def get_id(line):
 #On each line get the servernaem and the smtpfilter id, the 4th and 5th Column
 #We need to create a hash where these unique identifier can be stored.
 #Once that is done store them on the hash and for each new one check the hash so 
 #we don't store any duplicates. This will give us the total emails check on all the
 #sys_spam hosts.
+    matchObj = re.match(r'.*(\b\w+\-spam\b) (smtpfilter\[\d+\]).*',line);
+    email_id = '';
+    if matchObj:
+        print "It matched";
+        print "host > ",matchObj.group(1), " and id ",matchObj.group(2); 
+        email_id += matchObj.group(1);
+        email_id += matchObj.group(2);
+        insert_id(email_id);
+#        print "email id >", email_id;
+#        email_id += matchObj.group(1);
+#        email_id += matchObj.group(2);
+#        print "Potential id >",matchObj.group(1) += matchObj.group(2);
+    else:
+        print "Sorry no matched";
 #matchObj = re.match(r'       
-
-
+def insert_id(email_id):
+    if not email_id in email_list:
+        email_list.append(email_id);
 
 
 
