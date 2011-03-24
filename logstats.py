515ed4b0fd4ea9aec8d25f4a406c23f37cbbc5e5
chesarin <vargasces@gmail.com>
2011-03-24 14:06:59 -0400

    This code takes too long. Most likely in the concatanation and insertion to the table

--- logstats.py
+++ logstats.py
@@ -8,21 +8,6 @@ sentinel2=0;
 
 email_list = [];
 
-for line in udata:
-    sentinel1 += 1;
-    if re.search('smtpfilter',line):
-        sentinel2 += 1;
-        if sentinel2 == 1:
-            line1 = line;
-        elif sentinel2 == 2:
-            line2 = line;
-
-#print line1;
-#print line2;
-
-#print "The total number of lines is ",sentinel1 ," And the number of smtpfilter lines is ",sentinel2;
-
-udata.close();
 def get_id(line):
 #On each line get the servernaem and the smtpfilter id, the 4th and 5th Column
 #We need to create a hash where these unique identifier can be stored.
@@ -32,8 +17,8 @@ def get_id(line):
     matchObj = re.match(r'.*(\b\w+\-spam\b) (smtpfilter\[\d+\]).*',line);
     email_id = '';
     if matchObj:
-        print "It matched";
-        print "host > ",matchObj.group(1), " and id ",matchObj.group(2); 
+#        print "It matched";
+#        print "host > ",matchObj.group(1), " and id ",matchObj.group(2); 
         email_id += matchObj.group(1);
         email_id += matchObj.group(2);
         insert_id(email_id);
@@ -47,6 +32,28 @@ def get_id(line):
 def insert_id(email_id):
     if not email_id in email_list:
         email_list.append(email_id);
+
+for line in udata:
+    sentinel1 += 1;
+    if re.search('smtpfilter',line):
+        sentinel2 += 1;
+        get_id(line);
+#        sentinel2 += 1;
+#        if sentinel2 == 1:
+#            line1 = line;
+#        elif sentinel2 == 2:
+#            line2 = line;
+
+#print line1;
+#print line2;
+
+#print "The total number of lines is ",sentinel1 ," And the number of smtpfilter lines is ",sentinel2;
+
+udata.close();
+
+print "Total lines on file:> ",sentinel1;
+print "Total lines related with smtpfilter:> ",sentinel2;
+print "Total email scanned by smtpfilter:> ",len(email_list);
 
 
 
