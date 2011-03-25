86ff43dcebe64211e02df35ddad7c6b950210675
chesarin <vargasces@gmail.com>
2011-03-25 16:03:58 -0400

    latest updates, I think I can use match so that it is faster

--- logstats.py
+++ logstats.py
@@ -2,12 +2,6 @@
 import bz2;
 import re;
 
-udata = bz2.BZ2File("spamlog.20101222.bz2",'r');
-sentinel1=0;
-sentinel2=0;
-
-email_list = [];
-
 def get_id(line):
 #On each line get the servernaem and the smtpfilter id, the 4th and 5th Column
 #We need to create a hash where these unique identifier can be stored.
@@ -19,9 +13,14 @@ def get_id(line):
     if matchObj:
 #        print "It matched";
 #        print "host > ",matchObj.group(1), " and id ",matchObj.group(2); 
-        email_id += matchObj.group(1);
-        email_id += matchObj.group(2);
+        server_name = matchObj.group(1);
+        proc_id = matchObj.group(2);
+        email_id = server_name + proc_id;
         insert_id(email_id);
+#        insert_id(server_name+prc_id);
+#        email_id += matchObj.group(1);
+#        email_id += matchObj.group(2);
+#        insert_id(email_id);
 #        print "email id >", email_id;
 #        email_id += matchObj.group(1);
 #        email_id += matchObj.group(2);
@@ -33,11 +32,22 @@ def insert_id(email_id):
     if not email_id in email_list:
         email_list.append(email_id);
 
+udata = bz2.BZ2File("spamlog.20101222.bz2",'r');
+sentinel1=0;
+sentinel2=0;
+
+email_list = [];
+email_idPattern = re.compile(r'.*(\b\w+\-spam\b) (smtpfilter\[\d+\]).*');
 for line in udata:
     sentinel1 += 1;
-    if re.search('smtpfilter',line):
+    if email_idPattern.match(line):
+#        email_idPattern.groups
+#    if re.match(r'.*(\b\w+\-spam\b) (smtpfilter\[\d+\]).*',line).groups():
+#    if re.search('smtpfilter',line):
         sentinel2 += 1;
-        get_id(line);
+#        servername = email_idPattern.group(1) + email_idPattern.group(2);
+#        email_server = 
+#        get_id(line);
 #        sentinel2 += 1;
 #        if sentinel2 == 1:
 #            line1 = line;
@@ -46,14 +56,12 @@ for line in udata:
 
 #print line1;
 #print line2;
-
+udata.close();
 #print "The total number of lines is ",sentinel1 ," And the number of smtpfilter lines is ",sentinel2;
 
-udata.close();
-
 print "Total lines on file:> ",sentinel1;
-print "Total lines related with smtpfilter:> ",sentinel2;
-print "Total email scanned by smtpfilter:> ",len(email_list);
+print "Total lines related with unique smtpfilter is:> ",sentinel2;
+#print "Total email scanned by smtpfilter:> ",len(email_list);
 
 
 
